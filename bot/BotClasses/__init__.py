faa30e95a23e87798b1491268a0684671da404d4
Ilya Dobrynin <mr.woodysimpson@gmail.com>
2024-01-22 14:54:17 +0300

    new features

--- bot/BotClasses/__init__.py
+++ bot/BotClasses/__init__.py
@@ -1,5 +1,4 @@
 from bot.BotClasses.Command import *
 from bot.BotClasses.Message import *
 from bot.BotClasses.User import *
-from bot.BotClasses.Telegram import *
 from bot.BotClasses.Database_connection import connection, cursor
